849662c6cc090af2a35414bd2e76dd213ba7ee45
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-08 20:04:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+# number should only be divisible by 1 and itself 
+def prime?(num)
+  if num <= 1
+    return false
+  else
+    i = 2
+    while i < num - 1
+      if num % i == 0
+        return false
+      end
+      i += 1
+    end
+    return true
+  end
+end
